e86947f7386f6d0588d115fe853e18dd1c246b3c
MrEthan <ethan0ly@gmail.com>
2017-03-03 00:34:59 +0800

    包package的用法

---
+++ workspace/HelloWorld/src/com/ethan/food/Test.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.ethan.food;
+
+public class Test {
+	public static void main(String[] args)
+	{
+		Rice r = new Rice();
+		r.eat();
+		r.happyFood();
+		
+		Corn c = new Corn();
+		c.eat();
+		c.happyFood();
+	}
+}
